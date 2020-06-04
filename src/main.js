063a1d632c6a75b2490c52f11a740f4aff73c88f
jyoti attri <jyotiatri22@gmail.com>
2020-06-04 16:28:09 +0530

    added images and style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+// import jquery from "jquery";
+import "popper.js";
+// import Bootstrap from "bootstrap";
+import "bootstrap/dist/js/bootstrap";
 
 Vue.config.productionTip = false;
 
