1a5ae1a5b1195c285c3b1f6f100b0f8ef27380d6
jyoti attri <jyotiatri22@gmail.com>
2020-06-14 18:08:02 +0530

    changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="container">
+     <div class="main-menu d-none d-lg-block">
+                  <nav>
+                    <ul id="navigation">
+     <router-view></router-view>
+      <div>
+        <router-link to="/">
+        /Home
+        </router-link>
+        <router-link to="/About">
+        /About
+        </router-link>
+        <router-link to="/Services">
+        /Services
+        </router-link>
+        <router-link to="/Blog">
+        /Blog
+        </router-link>
+        </ul>
+                  </nav>
+                </div>
     </div>
 
 </template>
