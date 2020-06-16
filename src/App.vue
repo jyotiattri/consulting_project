e40217946008ad2b15095932f89a0dd3d8cdddf1
jyoti attri <jyotiatri22@gmail.com>
2020-06-16 12:20:24 +0530

    menu added

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,18 @@
      <div class="main-menu d-none d-lg-block">
                   <nav>
                     <ul id="navigation">
-     <router-view></router-view>
-      <div>
-        <router-link to="/">
-        /Home
-        </router-link>
-        <router-link to="/About">
-        /About
-        </router-link>
-        <router-link to="/Services">
-        /Services
-        </router-link>
-        <router-link to="/Blog">
-        /Blog
-        </router-link>
-        </ul>
+        <li><a
+            href="home.html"
+          >Home</li>
+
+        <li>
+          <a
+            href="about.html"
+          >
+            About Us
+          </a>
+        </li>
+      </ul>
                   </nav>
                 </div>
     </div>
